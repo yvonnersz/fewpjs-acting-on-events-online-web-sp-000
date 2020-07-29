a295ae302627c20a6852867a110dffcb2e660800
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 05:30:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let dodger = document.getElementById("dodger");
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
